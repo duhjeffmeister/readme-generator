831675f037a3a96ef272c69987f4df60a02218d3
Jeff Small <jeffmeister1990@gmail.com>
2022-06-26 16:07:44 -0400

    Added questions

---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateReadMe = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name? (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your name!');
+            return false;
+        }
+    }
+    },
+    {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username (Required)',
+    validate: githubInput => {
+        if (githubInput) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+        }
+    }
+    },
+    {
+    type: 'confirm',
+    name: 'confirmAbout',
+    message: 'Would you like to enter some information about yourself for an "About" section?',
+    default: true
+    },
+    {
+    type: 'input',
+    name: 'about',
+    message: 'Provide some information about yourself:',
+    when: ({ confirmAbout }) => confirmAbout
+    }];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
