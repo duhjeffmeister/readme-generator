9a44834ddb8120cd73e3920f6922a6ad1ee4a0e9
Jeff Small <jeffmeister1990@gmail.com>
2022-07-06 19:08:23 -0400

    Created license section function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License 📛
+
+      Copyright © ${license}. All rights reserved. 
+      
+      Licensed under the ${license} license.`
+
+    )
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
