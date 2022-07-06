e41b92a09f0a900ef298b8884d545ca739d049ab
Jeff Small <jeffmeister1990@gmail.com>
2022-07-06 19:06:15 -0400

    Created Badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ''
 }
 
 // TODO: Create a function that returns the license link
