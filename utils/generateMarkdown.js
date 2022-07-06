daa7280e72efb3253eb196a121915758aea575de
Jeff Small <jeffmeister1990@gmail.com>
2022-07-06 19:05:44 -0400

    Added Markdown Generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +15,60 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderBadge(data.license)}
 
+  ## Github URL 🌐
+  
+  [${data.github}](https://github.com/${data.github}/)
+  
+  ## Description 📝
+  
+  ${data.description}
+  
+  
+  ## Table of Contents 🗒
+  
+  * [Installations](#dependencies)
+  
+  * [Usage](#usage)
+  
+  ${renderLink(data.license)}
+  
+  * [Contributors](#contributors)
+  
+  * [Test](#test)
+  
+  
+  ## Installations (Dependencies) 💻
+  
+  To install dependencies, run these commands:
+  
+  \`\`\`
+  ${data.dependencies}
+  \`\`\`
+  
+  
+  ## Usage 🏆
+  
+  ${data.usage}
+  
+  ${renderSection(data.license)}
+  
+  
+  ## Contributors 😃
+  
+  ${data.contributors}
+  
+  Contact me at ${data.email}
+  
+  
+  ## Tests 🧪
+  
+  To run tests, run these commands:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`  
 `;
 }
 
