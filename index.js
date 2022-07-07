35c5046215034cbe300e1dfa5497b715de658552
Jeff Small <jeffmeister1990@gmail.com>
2022-07-06 21:08:06 -0400

    Finished questions in index.js

--- index.js
+++ index.js
@@ -6,43 +6,54 @@ const generateReadMe = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name? (Required)',
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log('Please enter your name!');
-            return false;
-        }
+        type: "input",
+        name: "github",
+        message: "What's your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe about your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the usage of this repo?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?",
     }
-    },
-    {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username (Required)',
-    validate: githubInput => {
-        if (githubInput) {
-            return true;
-        } else {
-            console.log('Please enter your GitHub username!');
-            return false;
-        }
-    }
-    },
-    {
-    type: 'confirm',
-    name: 'confirmAbout',
-    message: 'Would you like to enter some information about yourself for an "About" section?',
-    default: true
-    },
-    {
-    type: 'input',
-    name: 'about',
-    message: 'Provide some information about yourself:',
-    when: ({ confirmAbout }) => confirmAbout
-    }];
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
